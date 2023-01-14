8d1dbafe922aa044bf997838e77fcba33090418d
Blease Carson <bleasecarson@gmail.com>
2023-01-13 20:58:51 -0500

    Completed the program and added the applicable comments.

---
+++ tip-calculator.py
@@ -0,0 +1,16 @@
+
+# Greeting
+print("Welcome to the tip calculator.")
+# Captures the total cost of the bill. 
+bill = input("What was the total bill? ")
+# Captures how many patrons there are.
+num_of_diners = input("How many people are splitting this bill? ")
+# Captures the percentage amount the patrons wich to tip and then converts it to an integer.  
+tip_percentage = (int(input("What percentage tip would you like to leave your waiter? ")))/100
+# Calculates the total each patron is responsible for with 
+calculation = round((float(bill) + ((float(bill) * tip_percentage)))/int(num_of_diners), 2)
+
+print(f"Each person should pay ${calculation}")
+
+
+
